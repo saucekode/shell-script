79768f8bc33a509238a5992754dc03195cdb6d92
saucekode <saucekoder@gmail.com>
2024-12-10 12:12:56 +0100

    starting intermediate tasks

--- beginner/calculator.sh
+++ beginner/calculator.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-first_num="$1"
-second_num="$2"
+first_operand="$1"
+second_operand="$2"
 operator="$3"
 
 function simple_calculator(){
-    if [ "$operator" == "+" ];
-    then
-        result=$((first_num + $second_num))
-    elif [ "$operator" == "-" ];
-    then
-        result=$((first_num - $second_num))
-    elif [ "$operator" == "x" ];
-    then
-        result=$(($first_num * $second_num))
-    elif [ "$operator" == "/" ];
-    then
-        result=$(($first_num / $second_num))
+    if [ "$operator" == "+" ]; then
+        result=$((first_operand + second_operand))
+    elif [ "$operator" == "-" ]; then
+        result=$((first_operand - second_operand))
+    elif [ "$operator" == "x" ]; then
+        result=$((first_operand * second_operand))
+    elif [ "$operator" == "/" ]; then
+        result=$((first_operand / second_operand))
     fi
     
     echo $result
